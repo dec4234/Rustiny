108ede740e2953c16a9e0e659470b6e26a6fcb53
dec4234 <decarround04@gmail.com>
2022-02-12 22:42:24 -0500

    Include Example /LinkedProfiles/ Response

--- src/api/user/BungieUser.rs
+++ src/api/user/BungieUser.rs
@@ -32,3 +32,49 @@ impl DestinyPlatform {
         }
     }
 }
+
+/*
+{
+   "Response":{
+      "profiles":[
+         {
+            "dateLastPlayed":"2022-02-06T21:12:49Z",
+            "isOverridden":false,
+            "isCrossSavePrimary":false,
+            "crossSaveOverride":0,
+            "applicableMembershipTypes":[
+               3
+            ],
+            "isPublic":false,
+            "membershipType":3,
+            "membershipId":"4611686018468620320",
+            "displayName":"dec4234",
+            "bungieGlobalDisplayName":"dec4234",
+            "bungieGlobalDisplayNameCode":9904
+         }
+      ],
+      "bnetMembership":{
+         "supplementalDisplayName":"dec4234#9904",
+         "iconPath":"/img/profile/avatars/cc14.jpg",
+         "crossSaveOverride":0,
+         "isPublic":false,
+         "membershipType":254,
+         "membershipId":"17506516",
+         "displayName":"dec4234",
+         "bungieGlobalDisplayName":"dec4234",
+         "bungieGlobalDisplayNameCode":9904
+      },
+      "profilesWithErrors":[
+
+      ]
+   },
+   "ErrorCode":1,
+   "ThrottleSeconds":0,
+   "ErrorStatus":"Success",
+   "Message":"Ok",
+   "MessageData":{
+
+   }
+}
+
+ */
